b4aca9518bd609cda7c6e32b6b0b2ac207ecc441
newweborder007 <newweborder007@gmail.com>
2024-10-04 17:43:37 +0500

    refactor: update consistent capitalization handling for tokens

--- src/renderer/pages/Dashboard.tsx
+++ src/renderer/pages/Dashboard.tsx
@@ -6,10 +6,22 @@ const Dashboard: React.FC = () => {
   return (
     <>
       <Tabs aria-label="Default tabs" className="pt-4">
-        <Tabs.Item title={<FormattedMessage id="projects-view" />} id="projects-view">
+        <Tabs.Item
+          title={
+            <p className="capitalize">
+              <FormattedMessage id="projects-view" />
+            </p>
+          }
+        >
           <ProjectsTab />
         </Tabs.Item>
-        <Tabs.Item title={<FormattedMessage id="units-view" />} id="units-view">
+        <Tabs.Item
+          title={
+            <p className="capitalize">
+              <FormattedMessage id="units-view" />
+            </p>
+          }
+        >
           <div className="p-4">
             <UnitsTab />
           </div>
