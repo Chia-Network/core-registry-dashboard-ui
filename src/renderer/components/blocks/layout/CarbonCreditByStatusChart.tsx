20403705def813293965d007eef1bd43cc004796
newweborder007 <newweborder007@gmail.com>
2024-10-15 21:02:03 +0500

    refactor: update issuedCarbonStatusChart to handle year & use date-utils

--- src/renderer/components/blocks/layout/CarbonCreditByStatusChart.tsx
+++ src/renderer/components/blocks/layout/CarbonCreditByStatusChart.tsx
@@ -3,44 +3,27 @@ import { PieChart, Card, SkeletonCard, Select } from '@/components';
 import { useQueryParamState } from '@/hooks';
 import { FormattedMessage } from 'react-intl';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { generatePieChartData, pieChartOptionsBase } from '@/utils/chart-utils';
-import { useEffect, useState } from 'react';
+import { createChartDataWithSingleDataset, pieChartOptionsBase } from '@/utils/chart-utils';
+import { generateYearsRange } from '@/utils/date-utils';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-const borderColors = ['rgba(75, 192, 192, 1)', 'rgba(0, 43, 73, 1)', 'rgba(0, 103, 160, 1)'];
-const backgroundColors = ['rgba(75, 192, 192, 0.7)', 'rgba(0, 43, 73, 0.7)', 'rgba(0, 103, 160, 0.7)'];
-
-const generateLastTenYears = (): { label: string; value: string }[] => {
-  const currentYear = `${new Date().getFullYear()}`;
-  return Array.from({ length: 10 }, (_, index) => {
-    const year = parseInt(currentYear) - index;
-    return { label: year.toString(), value: `${year}` };
-  });
-};
 
 const CarbonCreditByStatusChart = () => {
-  const [unitStatus] = useQueryParamState('unitStatus', 'all');
-  const [vintageYear] = useQueryParamState('vintageYear', undefined);
-  const [selectedYear, setSelectedYear] = useState<string>(`${new Date().getFullYear()}`);
+  const [unitStatus] = useQueryParamState('carbonCreditUnitStatus', 'all');
+  const [vintageYear, setVintageYear] = useQueryParamState('vintageYear', undefined);
 
   const {
     data: carbonCreditByStatusData,
     isLoading: carbonCreditByStatusLoading,
     error: carbonCreditByStatusError,
-  } = useGetTonsCo2Query({ unitStatus, vintageYear: selectedYear });
-
-  useEffect(() => {
-    if (vintageYear) {
-      setSelectedYear(vintageYear);
-    }
-  }, [vintageYear]);
+  } = useGetTonsCo2Query({ unitStatus, vintageYear });
 
   const labels = carbonCreditByStatusData?.data.map((item: any) => item.unitStatus) || [];
   const datasetData = carbonCreditByStatusData?.data.map((item: any) => item.tonsCo2) || [];
-  const chartData = generatePieChartData(labels, datasetData, backgroundColors, borderColors, 'TonsCo2');
+  const chartData = createChartDataWithSingleDataset(labels, datasetData, 'TonsCo2');
 
   const handleYearChange = (value) => {
-    setSelectedYear(value);
+    setVintageYear(value);
   };
 
   if (carbonCreditByStatusLoading) {
@@ -65,11 +48,8 @@ const CarbonCreditByStatusChart = () => {
 
   return (
     <Card>
-      <p className="capitalize">
-        <FormattedMessage id={'carbon-credits-by-status'} />
-      </p>
       <div className="grid justify-end">
-        <Select name="year" options={generateLastTenYears()} initialValue={selectedYear} onChange={handleYearChange} />
+        <Select name="year" options={generateYearsRange(10)} initialValue={vintageYear} onChange={handleYearChange} />
       </div>
       <PieChart
         className="max-h-[450px]"
@@ -79,7 +59,7 @@ const CarbonCreditByStatusChart = () => {
           plugins: {
             ...pieChartOptionsBase.plugins,
             title: {
-              display: false,
+              ...pieChartOptionsBase.plugins.title,
               text: 'Carbon Credits by Status',
             },
           },
