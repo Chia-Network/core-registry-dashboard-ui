50f01d7b7b6330b3574b6388d4ee4b1661451816
newweborder007 <newweborder007@gmail.com>
2024-10-23 10:12:01 +0500

    refactor: unmount the dark mode toggle component

--- src/renderer/components/layout/Header.tsx
+++ src/renderer/components/layout/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { AppLogo, ConnectButton, ThemeModeSelector } from '@/components';
+import { AppLogo, ConnectButton } from '@/components';
 
 const Header: React.FC = () => {
   return (
     <div style={{ height: '64px' }}>
       <div className="border-t-8 border-green-400 pt-1 pb-1 w-screen h-16 bg-[#00242C] dark:bg-gray-800 dark:border-gray-600">
         {/* Expanded the container to full width and adjusted padding for larger breakpoints */}
-        <div className="flex justify-between items-center h-full px-4 lg:px-10 w-full">
+        <div className="flex items-center justify-between w-full h-full px-4 lg:px-10">
           {/* App logo with left alignment */}
           <div className="flex items-center h-full mr-5">
             <AppLogo width="100%" height="80%" type="" />
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
           {/* Right-aligned elements with explicit right margin on larger breakpoints */}
           <div className="flex items-center gap-5 text-white">
             <ConnectButton />
-            <ThemeModeSelector />
+            {/* <ThemeModeSelector /> */}
           </div>
         </div>
       </div>
